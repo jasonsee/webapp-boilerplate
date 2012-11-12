d6eb7e6f9b5aea76486718fb9ff5cd8345f277b2
Matt Dawson <matthewtdawson@gmail.com>
2012-11-12 12:28:58 -0500

    Add jasmine scaffolding.

---
+++ test/spec/app.js
@@ -0,0 +1,12 @@
+define(['app'], function (App) {
+    "use strict";
+
+    describe("App", function () {
+        it(
+            "should be defined",
+            function () {
+                expect(App).toBeDefined();
+            }
+        );
+    });
+});
