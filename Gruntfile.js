a06a511010f80280e43bd2b3130993d5f51f3878
Matt Dawson <matthewtdawson@gmail.com>
2012-11-15 22:42:11 -0500

    Fix path on test task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -188,7 +188,11 @@ module.exports = function(grunt) {
     });
 
     // Register local tasks.
-    grunt.registerTask('test', ['connect', 'jasmine']);
+    grunt.registerTask('test', function () {
+        // Test from the repo root instead of app root.
+        grunt.config.set('connect.base', '.');
+        grunt.task.run(['connect', 'jasmine']);
+    });
 
     grunt.registerTask('build', [
         'clean',
